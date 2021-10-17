922764444289571f824659163873df6cce29f68a
Sabyasachi Nandy <sabya.nandy14@gmail.com>
2021-10-17 14:33:54 +0530

    ADD | Add support for generic config based devices

--- homebridge/switch/index.js
+++ homebridge/switch/index.js
@@ -3,19 +3,6 @@ const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 
 let blueSwitchStatus = false;
-const portAC = new SerialPort('/dev/rfcomm0', {baudRate: 9600}).setEncoding('utf8');
-const portFAN = new SerialPort('/dev/rfcomm1', {baudRate: 9600}).setEncoding('utf8');
-
-const parserAC = portAC.pipe(new Readline({ delimiter: '\n' }));
-const parserFan = portFAN.pipe(new Readline({ delimiter: '\n' }));
-
-parserAC.on('data', data =>{
-    console.log('got word from arduino ac:', data);
-});
-parserFan.on('data', data =>{
-    console.log('got word from arduino fan:', data);
-});
-
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
@@ -25,23 +12,19 @@ module.exports = function (homebridge) {
 function blueSwitch(log, config) {
     this.log = log;
     this.name = config['name'];
+    this.port = config["port"]
+    this.devPort = new SerialPort(this.port, {baudRate: 9600}).setEncoding('utf8');
+    const parser = this.devPort.pipe(new Readline({delimiter: '\n'}));
+    parser.on('data', data => {
+        console.log(`Health of ${this.name} - ${data}`);
+    });
+
     this.service = new Service.Switch(this.name);
     this.informationService = new Service.AccessoryInformation();
-
     this.service
         .getCharacteristic(Characteristic.On)
         .on('get', this.getOn.bind(this))
         .on('set', this.setOn.bind(this));
-
-    setInterval(() => {
-        // Try to get health of ports, if port is dead, try to reconnect
-        try {
-
-
-        } catch (err) {
-            console.log("Err " + err.message)
-        }
-    }, 500);
 }
 
 blueSwitch.prototype.getServices = function () {
@@ -53,22 +36,14 @@ blueSwitch.prototype.getOn = function (next) {
 }
 
 blueSwitch.prototype.setOn = function (on, next) {
-    function extracted(selectedPort, value, deviceName) {
-        selectedPort.write(value, function (err) {
-            if (err) {
-                return console.log("Error on write: ", err.message);
-            }
-            blueSwitchStatus = value === '1';
-            console.log("[+] Message written: " + value + " on port " + deviceName);
-        });
-    }
-
-    let selectedPort = portAC;
-    if (this.name === 'Fan')
-        selectedPort = portFAN
-    if (on === true)
-        extracted(selectedPort, "1", this.name);
-    else if (on === false)
-        extracted(selectedPort, "0", this.name);
+    let value = '0';
+    if (on === true) value = '1';
+    this.devPort.write(value, function (err) {
+        if (err) {
+            return console.log("Error on write: ", err.message);
+        }
+        blueSwitchStatus = value === '1';
+        console.log(`Data written: ${value} to device ${this.name}`);
+    });
     next(null);
 }
