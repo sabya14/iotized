7263dad128a44ff09f7a7a7abaed3cfd66c2b2ed
sabyasachi.nandy <sabya.nandy14@gmail.com>
2022-08-10 11:15:51 +0530

    Changes | Update rc car code with non linear throttle mapping

--- arduino/rc/rc_car.ino
+++ arduino/rc/rc_car.ino
@@ -6,31 +6,37 @@
 #define STEER 18
 #define MOTOR 5
 
+#define Z_YAW 17
+#define Y_PITCH 16
+
 #include <ESP32Servo.h>
 #define sprint Serial.print
 #define sprintln Serial.println
+
+Servo steering;  // create servo object to control a servo
+Servo engine;
+
 
 int steer = 0;
 int motor = 0;
 bool killOff = false;
 bool turbo = false;
-int throtle;
-Servo steering;  // create servo object to control a servo
-Servo engine;
-
+int throttle;
+int mappedThrottle;
+ 
 int readChannel(int channelInput, int minLimit, int maxLimit, int defaultValue){
   int ch = pulseIn(channelInput, HIGH, 30000);
   if (ch < 200) return defaultValue;
   return map(ch, 1000, 2000, minLimit, maxLimit);
 }
-
+ 
 // Read the switch channel and return a boolean value
 bool readSwitch(byte channelInput, bool defaultValue){
   int intDefaultValue = (defaultValue)? 100: 0;
   int ch = readChannel(channelInput, 0, 100, intDefaultValue);
   return (ch > 50);
 }
-
+ 
 void setup(){
   Serial.begin(115200);
   steering.attach(STEER);
@@ -41,9 +47,9 @@ void setup(){
   pinMode(CH6, INPUT);
 
   Serial.print("Arming Test Starts in ");
-  engine.writeMicroseconds(700);
+  engine.writeMicroseconds(715);
   delay(5000);
-
+  
 }
 
 
@@ -54,40 +60,27 @@ void loop() {
    if (killOff == true) {
     // Get values for each channel
     steer = readChannel(CH4, 29, 151, 90);
-    throtle = readChannel(CH2, 0, 2000, 0);
-    if (!turbo) {
-      if (throtle >= 1250) {
-        throtle = 1250;
-      }
-    }
-    else {
-      if (throtle >= 1500) {
-        throtle = 1500;
-      }
-
-    }
-
-    if (throtle <= 1180) {
-      throtle = 700;
-    }
-
+    throttle = readChannel(CH2, 0, 2000, 0);
+    Serial.println("Actual throttle: ");
+    Serial.println(throttle);
+    mappedThrottle = 1278.789 + ((-361648700 - 1278.789)/(1 + pow((throttle/3.542685),2.515662)));
     Serial.print("Steer: ");
     Serial.print(steer);
     Serial.print(" | Throttle: ");
-    Serial.print(throtle);
+    Serial.print(mappedThrottle);
     Serial.print(" | KillOff: ");
     Serial.print(killOff);
     Serial.print(" | Turbo: ");
     Serial.print(turbo);
     Serial.println(" ");
     steering.write(steer);
-
-    engine.writeMicroseconds(throtle);
+    
+    engine.writeMicroseconds(mappedthrottle);
     delay(1);
   }
   else {
    Serial.println("Kill Switch Activated");
    steering.write(90);
-   engine.writeMicroseconds(700);
+   engine.writeMicroseconds(700);  
   }
 }
